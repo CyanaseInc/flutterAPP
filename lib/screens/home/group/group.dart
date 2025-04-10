535d56976be74bb6340c352ce0884dcb7a6744c5
Maliabu <giramiapatricia61@gmail.com>
2025-04-10 17:59:46 +0300

    websocket chat

--- lib/screens/home/group/group.dart
+++ lib/screens/home/group/group.dart
@@ -1,3 +1,5 @@
+import 'package:cyanase/helpers/web_db.dart';
+import 'package:cyanase/helpers/websocket.dart';
 import 'package:flutter/material.dart';
 import 'chat_list.dart';
 import 'package:cyanase/theme/theme.dart';
@@ -8,16 +10,34 @@ class GroupsTab extends StatefulWidget {
 }
 
 class _GroupsTabState extends State<GroupsTab> {
+  dynamic userToken = '';
   // Key to refresh the ChatList
   final GlobalKey<ChatListState> _chatListKey = GlobalKey();
 
+  getToken() async {
+    await WebSharedStorage.init();
+    var existingProfile = WebSharedStorage();
+    var token = existingProfile.getCommon('token');
+    setState(() {
+      userToken = token;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getToken(); // Call getToken only once during initialization
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    print('USERTOKEN: $userToken');
+    return const Scaffold(
       backgroundColor: white, // Set the background color to white
-      body: ChatList(
-        key: _chatListKey, // Pass the key to ChatList
-      ),
+      // body: ChatList(
+      //   key: _chatListKey, // Pass the key to ChatList
+      // ),
+      body: ChatScreen(),
     );
   }
 }
