0aa8e057c0e2ae7ae2d540b81f0323819657bb49
cyanaseinc <cyanaseinvestors@gmail.com>
2025-06-14 08:56:55 +0300

    dep_fixed

--- lib/screens/home/group/invite.dart
+++ lib/screens/home/group/invite.dart
@@ -40,7 +40,7 @@ class _InviteScreenState extends State<InviteScreen> {
   }
 
   String _generateServerLink() {
-    return "${'http://localhost:3000'}/join-group/${widget.inviteCode}";
+    return "${'https://cyanase.com'}/join-group/${widget.inviteCode}";
   }
 
   void _handleResetLink() {
